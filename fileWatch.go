d215b3901f542441ee66e94a81e0fb17d3f64d18
wawa <gengkang@xnw.com>
2017-05-14 16:24:00 +0800

    优化工具可用性

--- fileWatch.go
+++ fileWatch.go
@@ -3,6 +3,7 @@ package main
 import (
     "github.com/howeyc/fsnotify"
     "log"
+    "os"
 )
 
 type monitor struct {
@@ -10,19 +11,48 @@ type monitor struct {
 }
 
 func main() {
+    if len(os.Args) < 2 {
+        log.Println("no dir param to watch")
+        os.Exit(1)
+    }
+    dirName := os.Args[1]
+    fi,err := os.Stat(dirName)
+    if err != nil {
+        log.Println("open stram failed")
+        os.Exit(1)
+    }
+
+    if !fi.IsDir() {
+        log.Println("dir:", dirName , "not exist")
+        os.Exit(1)
+    }
     M, err := NewMonitor()
     if err != nil {
         log.Println(err)
         return
     }
     M.Do()
-    M.watch.Watch("/ssd/gengkang/.bashrc")
+    M.watch.Watch(dirName)
     select {}
 }
 
 func NewMonitor() (monitor, error) {
     Mon, err := fsnotify.NewWatcher()
     return monitor{Mon}, err
+}
+
+func fileSize(fileName string) {
+    fi, err := os.Stat(fileName)
+
+    if err != nil {
+
+        if os.IsNotExist(err) {
+            log.Println("file not exist")
+        }
+        return
+    }
+
+    log.Println(fi.Size)
 }
 
 func (self monitor) Do() {
@@ -32,10 +62,11 @@ func (self monitor) Do() {
             case w := <-self.watch.Event:
                 log.Println(w)
                 if w.IsModify() {
+                    fileSize(w.Name)
                     continue
                 }
                 if w.IsDelete() {
-                    log.Println("文件被删除.")
+                    log.Println("文件", w.Name, "被删除.")
                     continue
                 }
                 if w.IsRename() {
