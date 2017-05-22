f5c3a900c61633683ccf139847df65c05ee4217c
gengkang <gengkang@xnw.com>
2017-05-22 14:55:59 +0800

    优化脚本

--- jane.go
+++ jane.go
@@ -1,58 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"github.com/howeyc/fsnotify"
 	"log"
 	"net/smtp"
 	"os"
 	"strings"
 	"encoding/base64"
+	"path"
 )
 
-type monitor struct {
-	watch *fsnotify.Watcher
+func main() {
+	watchRoot()
+	select {}
 }
 
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("缺少参数，请 这样使用：jane.exe 目录 邮箱")
-		fmt.Println("no file/dirtory param to watch")
-		os.Exit(1)
+func watchRoot() {
+	rootDir := "/Users/xnw/gopath/src/go-file-watcher/test"
+	if len(os.Args) > 1 {
+		rootDir = os.Args[1]
 	}
-	fileName := os.Args[1]
-	_, err := os.Stat(fileName)
+
+	startWatch(rootDir)
+}
+
+func startWatch(dir string)  {
+	_, err := os.Stat(dir)
 	if err != nil {
-		log.Println("open stream failed")
-		os.Exit(1)
+		log.Println("open stream " + dir + " failed")
+		return
 	}
-	M, err := NewMonitor()
+	Watcher, err := fsnotify.NewWatcher()
+
 	if err != nil {
 		log.Println(err)
 		return
 	}
-	M.Do()
-	M.watch.Watch(fileName)
-	select {}
-}
-
-func NewMonitor() (monitor, error) {
-	Mon, err := fsnotify.NewWatcher()
-	return monitor{Mon}, err
-}
-
-func fileSize(fileName string) int64 {
-	fi, err := os.Stat(fileName)
-
-	if err != nil {
-		if os.IsNotExist(err) {
-			log.Println("file not exist")
-		}
-
-		return 0
-	}
-
-	return fi.Size()
+	go listen(Watcher)
+	Watcher.Watch(dir)
 }
 
 func sendToMail(to, subject, body, mailtype string) error {
@@ -74,47 +59,44 @@ func sendToMail(to, subject, body, mailtype string) error {
 func SendEmail(fileName string) {
 	sub := "亲爱的，化合物分析结束了"
 	content := "已经生成了" + fileName + "文件"
-	if os.Args[2] == "no" {
-		return
-	}
-	err := sendToMail(os.Args[2], sub, content, "html")
+	err := sendToMail("fltcsong@163.com", sub, content, "html")
 	if err != nil {
 		log.Println("send email failed")
-        return
+        	return
 	}
 
     log.Println("邮件已发送")
 }
 
-func (self monitor) Do() {
-	go func() {
-		for {
-			select {
-			case w := <-self.watch.Event:
-				if w.IsModify() {
-					log.Println(w.Name, "is changed, filesize is:", fileSize(w.Name))
-					continue
+func isDir(pathname string) bool {
+	fi, err := os.Stat(pathname)
+	if err != nil {
+		return false
+	}
+
+	return fi.IsDir()
+}
+
+func listen(self *fsnotify.Watcher) {
+	for {
+		select {
+		case w := <-self.Event:
+			log.Println(w)
+			if w.IsCreate() {
+				if strings.HasSuffix(strings.ToLower(w.Name), ".htm") && strings.Contains(strings.ToLower(w.Name), "output") {
+					log.Println("化合物生成结束")
+					SendEmail(w.Name)
+					self.RemoveWatch(path.Dir(w.Name))
+					break
 				}
-				if w.IsDelete() {
-					log.Println(w.Name, "is deleted")
-					continue
+
+				if isDir(w.Name) {
+					log.Println("add watch on ", w.Name, " ...")
+					self.Watch(w.Name)
 				}
-				if w.IsRename() {
-					log.Println(w.Name, "is renamed")
-					self.watch.RemoveWatch(w.Name)
-					continue
-				}
-				if w.IsCreate() {
-					log.Println(w.Name, "is created filesize is:", fileSize(w.Name))
-					if strings.HasSuffix(w.Name, ".html") {
-						log.Println("化合物生成结束")
-						SendEmail(w.Name)
-					}
-					continue
-				}
-			case err := <-self.watch.Error:
-				log.Fatalln(err)
 			}
+		case err := <-self.Error:
+			log.Fatalln(err)
 		}
-	}()
+	}
 }
